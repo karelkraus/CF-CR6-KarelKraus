002439b0afd95b591a68d310c1bb71d51779909a
Karel Kraus <kaja.kraus@gmail.com>
2020-10-16 14:33:59 +0200

    bonus content

--- script/script.ts
+++ script/script.ts
@@ -5,12 +5,14 @@ class Locations {
 	address;
 	zip;
 	pic;
+	date;
 
-	constructor (name, address, zip, pic) {
+	constructor (name, address, zip, pic, date) {
 		this.name = name;
 		this.address = address;
 		this.zip = zip;
 		this.pic = pic;
+		this.date = date
 
 		allLocations.push(this);
 	}
@@ -21,6 +23,7 @@ class Locations {
 				  <img src=${this.pic} class="card-img-top rounded" alt="...">
 				  <div class="card-body">
 				    <h5 class="card-title">${this.name}</h5>
+				    <p><small>${this.date}</small></p>
 				  </div>
 				  <ul class="list-group list-group-flush Information">
 				    <li class="list-group-item bg-light">${this.zip}</li>
@@ -31,14 +34,15 @@ class Locations {
 		 `
 	}
 
+
 }
 
 class Restaurants extends Locations {
 	type;
 	phone;
 	web;
-	constructor (name, address, zip, pic, type, phone, web) {
-		super(name, address, zip, pic);
+	constructor (name, address, zip, pic, date, type, phone, web) {
+		super(name, address, zip, pic, date);
 		this.type = type;
 		this.phone = phone;
 		this.web = web;
@@ -52,6 +56,7 @@ class Restaurants extends Locations {
 				  <img src=${this.pic} class="card-img-top rounded" alt="...">
 				  <div class="card-body">
 				    <h5 class="card-title">${this.name}</h5>
+				    <p><small>${this.date}</small></p>
 				  </div>
 				  <ul class="list-group list-group-flush Information">
 				  	<li class="list-group-item bg-light">${this.type}</li>
@@ -70,8 +75,8 @@ class Events extends Locations {
 	eventDate;
 	eventTime;
 	price;
-	constructor (name, address, zip, pic, eventDate, eventTime, price) {
-		super(name, address, zip, pic);
+	constructor (name, address, zip, pic, date, eventDate, eventTime, price) {
+		super(name, address, zip, pic, date);
 		this.eventDate = eventDate;
 		this.eventTime = eventTime;
 		this.price = price;
@@ -85,6 +90,7 @@ class Events extends Locations {
 				  <img src=${this.pic} class="card-img-top rounded" alt="...">
 				  <div class="card-body">
 				    <h5 class="card-title">${this.name}</h5>
+				    <p><small>${this.date}</small></p>
 				  </div>
 				  <ul class="list-group list-group-flush Information">
 				  	<li class="list-group-item bg-light">${this.eventDate} at ${this.eventTime}</li>
@@ -98,18 +104,30 @@ class Events extends Locations {
 	}
 }
 
-new Locations ("Karlskirche", "1010 Wien", "Karlsplatz 1", "img/place.jpg")
-new Locations ("Stephansdom", "1010 Wien", "Stephansplatz 1", "img/place2.jpg")
-new Locations ("Schloss Belvedere", "1030 Wien", "Prinz Eugen-Straße 27", "img/place3.jpg")
-new Locations ("Schönbrunn", "1130 Wien", "Schönbrunner Schloßstraße 47", "img/place4.jpg")
-new Restaurants ("Sixta", "1050 Wien", "Schönbrunner Straße 21", "img/rest.jpg", "östereichisch", "+43 1 58 528 56", "http://www.sixta-restaurant.at/")
-new Restaurants ("Le Mjam", "1030 Wien", "Irgendwelche Straße 1", "img/rest2.jpg", "französich", "+43 1 52 538 16", "http://www.lemjam.at/")
-new Restaurants ("Eat it", "1100 Wien", "Wiedersehen Straße 13", "img/rest3.jpg", "amerikanisch", "+43 1 22 838 46", "http://www.eatit.at/")
-new Restaurants ("Bongiorno", "1020 Wien", "Essengasse 4", "img/rest4.jpg", "italienisch", "+43 1 32 908 14", "http://www.bongiorno.at/")
-new Events ("Lenny Kravitz", "1150 Wien", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/event.jpg", "02/04/2021" , "20:00", "50") 
-new Events ("DJ Bobo", "1150 Wien", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/event2.jpg", "19/08/2021" , "20:00", "50") 
-new Events ("Cinema", "1030 Wien", "Johannesgasse 4", "img/event3.jpg", "28/02/2021" , "18:00", "50")
-new Events ("Mike's wedding", "1010 Wien", "Stephansplatz 1", "img/event4.jpg", "12/06/2021" , "12:00", "0")  
+new Locations ("Karlskirche", "1010 Wien", "Karlsplatz 1", "img/place.jpg", "2019-06-14 14:03")
+new Locations ("Stephansdom", "1010 Wien", "Stephansplatz 1", "img/place2.jpg", "2020-08-14 14:03:49")
+new Locations ("Schloss Belvedere", "1030 Wien", "Prinz Eugen-Straße 27", "img/place3.jpg", "2019-12-14 14:03")
+new Locations ("Schönbrunn", "1130 Wien", "Schönbrunner Schloßstraße 47", "img/place4.jpg", "2019-05-12 14:03")
+new Restaurants ("Sixta", "1050 Wien", "Schönbrunner Straße 21", "img/rest.jpg", "2019-07-23 13:29", "östereichisch", "+43 1 58 528 56", "http://www.sixta-restaurant.at/")
+new Restaurants ("Le Mjam", "1030 Wien", "Irgendwelche Straße 1", "img/rest2.jpg", "2019-02-03 11:38", "französich", "+43 1 52 538 16", "http://www.lemjam.at/")
+new Restaurants ("Eat it", "1100 Wien", "Wiedersehen Straße 13", "img/rest3.jpg", "2018-10-19 19:21", "amerikanisch", "+43 1 22 838 46", "http://www.eatit.at/")
+new Restaurants ("Bongiorno", "1020 Wien", "Essengasse 4", "img/rest4.jpg", "2020-05-29 17:56", "italienisch", "+43 1 32 908 14", "http://www.bongiorno.at/")
+new Events ("Lenny Kravitz", "1150 Wien", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/event.jpg", "2020-03-09 08:09", "02/04/2021" , "20:00", "50") 
+new Events ("DJ Bobo", "1150 Wien", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "img/event2.jpg", "2019-11-20 11:21", "19/08/2021" , "20:00", "50") 
+new Events ("Cinema", "1030 Wien", "Johannesgasse 4", "img/event3.jpg", "2020-09-14 12:10", "28/02/2021" , "18:00", "50")
+new Events ("Mike's wedding", "1010 Wien", "Stephansplatz 1", "img/event4.jpg", "2019-10-11 17:03", "12/06/2021" , "12:00", "0")  
 
 
 allLocations.forEach(something => $(".locations").append(something.render()))
+
+allLocations.sort(function(a, b) {
+  return  +new Date(b.date) - +new Date(a.date);
+})
+
+allLocations.forEach(something => $(".locationsDesc").append(something.render()))
+
+allLocations.sort(function(a, b) {
+  return  +new Date(a.date) - +new Date(b.date);
+})
+
+allLocations.forEach(something => $(".locationsAsc").append(something.render()))
